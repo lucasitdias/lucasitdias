8dec103368331720377b6ce1ffe10cebf9ceaa51
Lucas Dias <160205490+lucasitdias@users.noreply.github.com>
2026-03-07 03:28:50 -0300

    Update console log message for SVG generation

--- generate-cards.js
+++ generate-cards.js
@@ -14,7 +14,7 @@ if (!fs.existsSync("assets")) {
 
 repos.forEach(repo => {
 
-  const svg = `
+const svg = `
 <svg xmlns="http://www.w3.org/2000/svg" width="400" height="120">
 <rect width="100%" height="100%" fill="#0d1117" rx="15"/>
 <text x="20" y="50" fill="#58a6ff" font-size="20" font-family="Arial">
@@ -26,8 +26,8 @@ github.com/lucasitdias/${repo}
 </svg>
 `;
 
-  fs.writeFileSync(`assets/${repo}.svg`, svg);
+fs.writeFileSync(`assets/${repo}.svg`, svg);
 
 });
 
-console.log("Cards gerados");
+console.log("SVGs gerados");
